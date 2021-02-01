2e8c47a318bcbb013c026ab3784ba5323282364c
HarshadJariwala <harshadjariwala1998@gmail.com>
2021-02-01 11:58:11 +0530

    update

--- src/Helpers/appConfig.js
+++
@@ -1,8 +0,0 @@
-const appConfig = {
-    baseUrl: "http://app.membroz.com/api/",
-    headers: {
-        'Content-Type': 'application/json',
-    }
-}
-
-export default appConfig;
--- src/Helpers/appConfigReg.js
+++
@@ -1,9 +0,0 @@
-const appConfigReg = {
-    baseUrl: "http://app.membroz.com/api/",
-    headers: {
-        'Content-Type': 'application/json',
-        'authkey': "5ff6957fb638dd6a777f049c"
-    }
-}
-
-export default appConfigReg;
