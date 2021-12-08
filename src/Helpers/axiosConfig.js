34e82b440160a599f7d13d07e1a4045c56d86ec1
HarshadJariwala <harshadjariwala1998@gmail.com>
2021-12-08 13:02:15 +0530

    update

--- src/Helpers/axiosConfig.js
+++ src/Helpers/axiosConfig.js
@@ -1,8 +1,11 @@
 import appConfig from './appConfig';
+var date = new Date();
+var timezone = date.getTimezoneOffset();
 
 function axiosConfig(token) {
     if (token) {
         appConfig.defaults.headers.common['authkey'] = token;
+        appConfig.defaults.headers.common['timezone'] = timezone;
     } else {
         delete appConfig.defaults.headers.common['authkey'];
     }
