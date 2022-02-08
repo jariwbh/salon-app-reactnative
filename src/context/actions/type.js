85de89a9f374cf7da0f4717a0697e9c5c00a044c
HarshadJariwala <harshadjariwala1998@gmail.com>
2022-02-08 17:15:56 +0530

    update

--- src/context/actions/type.js
+++ src/context/actions/type.js
@@ -1,16 +1,22 @@
 
 //AUTH USER KEY NAME
-export const AUTHUSER = '@authusersalon'
-export const REMOTEDATA = '@remotedata'
-export const AUTHUSERINFO = '@authuserinfosalon'
-export const AUTHREMBERUSERINFO = '@authremberuserinfosalon'
-export const AUTHUSERBRANCH = '@authuserbranch'
-export const STARTUP = '@startup'
+export const AUTHUSER = '@authusersalon';
+export const REMOTEDATA = '@remotedata';
+export const AUTHUSERINFO = '@authuserinfosalon';
+export const AUTHREMBERUSERINFO = '@authremberuserinfosalon';
+export const AUTHUSERBRANCH = '@authuserbranch';
+export const STARTUP = '@startup';
 export const CLOUD_URL = 'https://api.cloudinary.com/v1_1/membroz/upload';
 export const UPLOAD_PRESET = 'egi2wepr';
 export const DEFAULTPROFILE = 'https://res.cloudinary.com/membroz/image/upload/v1637309115/Mobile%20Public%20Image/ic_account_mgu7ie.png';
-export const APPNAME = 'Cocoon Medical Spa'
+export const APPNAME = 'Jeskir';
 export const USERKEY = '6130a1b1bfd7602f90774da2';
 export const DEFAULTUSER = 'defaultuser';
-export const DefaultImage = 'https://res.cloudinary.com/membroz/image/upload/v1642743195/Cocoon%20Mobile%20App/logo_norzqs.png';
-//export const DefaultImage = 'https://res.cloudinary.com/membroz/image/upload/v1639641450/Cocoon%20Mobile%20App/logo2_rzpyeq.png';
+export const DefaultImage = 'https://res.cloudinary.com/membroz/image/upload/v1641989921/Jeskir%20Mobile%20App/playstore-removebg-preview_hgvxt5.png';
+
+//Jeskir
+//export const DefaultImage = 'https://res.cloudinary.com/membroz/image/upload/v1641989921/Jeskir%20Mobile%20App/playstore-removebg-preview_hgvxt5.png';
+
+//Cocoon Medical Spa
+// export const DefaultImage = 'https://res.cloudinary.com/membroz/image/upload/v1642743195/Cocoon%20Mobile%20App/logo_norzqs.png';
+//export const DefaultImage2 = 'https://res.cloudinary.com/membroz/image/upload/v1639641450/Cocoon%20Mobile%20App/logo2_rzpyeq.png';
